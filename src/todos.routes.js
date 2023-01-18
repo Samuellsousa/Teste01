9504c47bb278e060879fb20b2e74a6e8c73bdd8e
Samuellsousa <99738166+Samuellsousa@users.noreply.github.com>
2023-01-18 01:13:42 -0300

    rotas das paginas

---
+++ src/todos.routes.js
@@ -0,0 +1,19 @@
+const { response } = require("express");
+const express = require("express")
+
+const allTodos = [{nome: "aaaa", status:false}];
+const todosRoutes = express.Router();
+
+
+todosRoutes.post("/todos", (request, response) => {
+    const { name } = request.body;
+    allTodos.push({ name, status: false});
+    return response.status(201).json(allTodos);
+});
+
+todosRoutes.get("/todos", (request, response) =>{
+  return response.status(200) .json(allTodos)
+}) 
+
+
+module.exports = todosRoutes;
